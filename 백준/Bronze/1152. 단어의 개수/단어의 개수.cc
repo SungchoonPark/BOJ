778c578166c0ca08131d0d2ce480ef9610e183e1
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2022-03-09 19:05:44 +0900

    Update 단어의 개수.cc

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -1,17 +1,27 @@
-#include <iostream>
-#include <string> 
-using namespace std;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-int main(void){
-	int count = 0;
-	string arr;
-	getline(cin, arr);
-	
-	for(int i=1; i<arr.size(); i++){
-		if(arr[i] == ' '){
-			count ++;
-		}
-	}
-	if(arr.back() == ' ') cout << count;
-	else cout << count + 1;
-} 
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int count = 0;
+        int preCh = 32; // 아스키 코드 32는 공백문자를 뜻함
+        int str;
+        while(true) {
+            str = System.in.read();
+
+            if (str == 32) { // 공백인 경우
+                if(preCh != 32) count++;
+            } else if (str == 10) { // /n 인 경우
+                if(preCh != 32) count++;
+                break;
+            }
+
+            preCh = str;
+        }
+
+        System.out.println(count);
+    }
+}
