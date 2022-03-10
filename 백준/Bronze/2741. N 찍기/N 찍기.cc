b3727ef7b9892e513d71d63bec61c640c664118f
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2022-03-10 11:42:13 +0900

    Update N 찍기.cc

--- 백준/Bronze/2741. N 찍기/N 찍기.cc
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -7,15 +7,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringBuilder를 사용하여 출력을 해야 시간이 더욱 감축되네용
+        StringBuilder sb = new StringBuilder();
         int value = Integer.parseInt(br.readLine());
 
         for(int i =1; i<=value; i++) {
-            if(i==value) {
-                System.out.printf("%d", i);
-
-            } else {
-                System.out.println(i);
-            }
+            sb.append(i).append("\n");
         }
+        br.close();
+        System.out.println(sb);
     }
 }
