fa3ac30b63363a4185a4a2da1ba2e9d5c2a47083
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2022-03-10 11:59:58 +0900

    Update 숫자의 합.cc

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -1,18 +1,26 @@
-#include <iostream>
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-using namespace std;
+public class Main {
 
-int n, sum=0;
-string number;
-int main(void){
-	cin >> n;
-	cin >> number;
-	
-	for(int i=0; i<number.size(); i++){
-		sum += number[i] - '0';
-	}
-	
-	cout << sum;
-	
-	return 0;
+    public static void main(String[] args) throws IOException {
+        // 입력받은 문자열을 첫째줄에서 입력받은 거만큼 나눠서 정수로 바꾼다음 더하면 될듯
+        // 라고 생각하고 시작했지만 별로임.
+        // 그냥 substring 써서 한글자씩 더해주면 매우 간단한일
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        String inputNum = br.readLine();
+        int sum = 0;
+
+        for (int i =1; i<=n; i++) {
+            sum += Integer.parseInt(inputNum.substring(i-1, i));
+        }
+
+        System.out.println(sum);
+
+
+    }
 }
