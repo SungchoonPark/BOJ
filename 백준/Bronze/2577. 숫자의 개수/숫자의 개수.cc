7debc34faf259ebd5600e9541cac912483bcc6df
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2022-03-20 17:45:39 +0900

    숫자의개수.java

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -1,20 +1,30 @@
-#include <iostream>
-using namespace std;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-int A,B,C;
-int arr[10];
-int main(void){
-	cin >> A >> B >> C;
-	int res = A*B*C;
-	while(1){
-		int tmp = res%10;
-		arr[tmp]++;
-		res /= 10;
-		if(res == 0){
-			break;
-		}
-	}
-	for(int i=0; i<10; i++){
-		cout << arr[i] << "\n";
-	}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] count = new int[10];
+
+        int[] inputNum = new int[3];
+        for(int i = 0; i < 3; i++) {
+            inputNum[i] = Integer.parseInt(br.readLine());
+        }
+
+        int result = inputNum[0] * inputNum[1] * inputNum[2];
+
+        String num = Integer.toString(result);
+
+        for(int i = 0; i < num.length(); i++) {
+            String tmp = num.substring(i, i+1);
+            count[Integer.parseInt(tmp)]++;
+        }
+
+        for (int j : count) {
+            System.out.println(j);
+        }
+
+    }
 }
