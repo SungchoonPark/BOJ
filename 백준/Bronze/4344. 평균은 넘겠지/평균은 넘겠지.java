a62780af07b7e8acee7155c3b040005fe1d5288b
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2022-03-25 11:03:19 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 216 ms, Memory: 20748 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.java
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.java
@@ -3,40 +3,44 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Scanner sc = new Scanner(System.in);
-        int c= sc.nextInt();
-        
-        for(int i =0; i<c; i++) {
-            int[] n = new int[sc.nextInt()];
-            int sum=0;
-            int avg=0;
-            int cnt=0;
-            float stAvg = 0;
-            
-            for(int j=0; j<n.length; j++) {
-                n[j] = sc.nextInt();
-                sum += n[j];
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num; i++) {
+
+            double sum = 0;
+            int index = 0;
+            List<String> scoreList = new ArrayList<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            while (st.hasMoreTokens()) {
+                scoreList.add(st.nextToken());
+                if (index > 0) {
+                    sum += Double.parseDouble(scoreList.get(index));
+                }
+                index++;
             }
-            
-            avg = sum/(n.length);
-            
-            for(int k=0; k<n.length; k++) {
-                if(n[k] > avg){
-                    cnt++;
+
+            double avg = sum / Double.parseDouble(scoreList.get(0));
+
+            double good = 0;
+
+            for (int k = 1; k < scoreList.size(); k++) {
+                if (Double.parseDouble(scoreList.get(k)) > avg) {
+                    good++;
                 }
             }
-            
-            stAvg = (float) cnt/n.length*100;
-      
-            System.out.println(String.format("%.3f", stAvg)+"%");
+
+            double result = good / Double.parseDouble(scoreList.get(0)) * 100;
+            System.out.println(String.format("%.3f", result)+"%");
         }
+
     }
 }
 
