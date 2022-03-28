eb6f041cb23a66f386b82e5f0613eac9189cc250
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2022-03-28 15:14:53 +0900

    부녀회장이 될테야.java

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
@@ -1,29 +1,42 @@
-#include <iostream>
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-using namespace std;
+public class Main {
 
-int testNum, n, k, arr[15][15];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-int main(void){
-	int sum = 0;
-	cin >> testNum;
-	for(int i=0; i<15; i++){
-		for(int j=1; j<15; j++){
-			if(i==0){
-				arr[i][j] = j; 
-			}
-			else{
-				for(int k=1; k<=j; k++){
-					sum += arr[i-1][k];
-				}
-				arr[i][j] = sum;
-				sum = 0;
-			}
-		}
-	}
-	for(int i=0; i<testNum; i++){
-		cin >> k;
-		cin >> n;
-		cout << arr[k][n] << "\n";
-	}
+        int num = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[15][15];
+
+        //층
+        for(int i=0; i<15; i++) {
+            // 호
+            for(int j=0; j<14; j++) {
+                if(i == 0) {
+                    // 0층인 경우
+                    arr[i][j] = j+1;
+                } else {
+                    // 0층이 아닌경우
+                    if(j == 0) {
+                        // 1호인 경우
+                        arr[i][j] = 1;
+                    } else {
+                        // 1호가 아닌경우
+                        arr[i][j] = arr[i][j-1] + arr[i-1][j];
+                    }
+                }
+            }
+        }
+
+        for(int i=0; i<num; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(arr[k][n-1]);
+        }
+    }
+
 }
+
