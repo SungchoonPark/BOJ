be3107a150cc2f2af58c345a5adea94c364c4445
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2022-04-03 23:28:14 +0900

    Update 단어 뒤집기.java

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -9,16 +9,32 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<num; i++) {
-            StringBuilder sb = new StringBuilder();
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            while(st.hasMoreTokens()) {
-                String s = st.nextToken();
-                StringBuffer strBuff = new StringBuffer(s);
-                String reverse = strBuff.reverse().toString();
-                sb.append(reverse).append(" ");
-            }
-            System.out.println(sb);
+        // 584ms
+       for(int i=0; i<num; i++) {
+           StringBuilder sb = new StringBuilder();
+           StringTokenizer st = new StringTokenizer(br.readLine());
+           while(st.hasMoreTokens()) {
+               String s = st.nextToken();
+               StringBuffer strBuff = new StringBuffer(s);
+               String reverse = strBuff.reverse().toString();
+               sb.append(reverse).append(" ");
+           }
+           System.out.println(sb);
+       }
+
+        // 1028ms
+//         for(int i=0; i<num; i++) {
+//             StringBuilder sb = new StringBuilder();
+//             StringTokenizer st = new StringTokenizer(br.readLine());
+//             while(st.hasMoreTokens()) {
+//                 String s = st.nextToken();
+//                 String reverse = "";
+//                 for(int j=s.length()-1; j>=0; j--) {
+//                     reverse += s.charAt(j);
+//                 }
+//                 sb.append(reverse).append(" ");
+//             }
+//             System.out.println(sb);
         }
     }
 }
