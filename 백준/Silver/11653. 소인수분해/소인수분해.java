3d736ca705d54c313a95c14b1699d508aa22e9ad
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2022-04-20 11:49:19 +0900

    [Silver V] Title: 소인수분해, Time: 192 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/11653. 소인수분해/소인수분해.java
+++ 백준/Silver/11653. 소인수분해/소인수분해.java
@@ -10,60 +10,19 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int num = Integer.parseInt(br.readLine());
-        int tmp = num;
 
-        if(num == 1) {
-            return;
-        }
-
-        boolean[] arr = new boolean[num + 1];
-        arr[0] = true;
-        arr[1] = true;
-        // 에라토스테네스의 채
-        for (int i = 2; i * i < num + 1; i++) {
-            for (int j = i * i; j < num + 1; j += i) {
-                arr[j] = true;
+        for(int i=2; i*i <= num; i++) {
+            while(num%i == 0) {
+                sb.append(i).append('\n');
+                num /= i;
             }
         }
 
-        List<Integer> prime = new ArrayList<>();
-        for(int i=2; i<num+1; i++) {
-            if (!arr[i]) {
-                prime.add(i);
-            }
+        if (num != 1) {
+            sb.append(num);
         }
 
-        List<Integer> result = new ArrayList<>();
-        int i = 0;
-        while(true) {
-            if(tmp % prime.get(i) == 0) {
-                if(tmp / prime.get(i) == 1) {
-                    result.add(prime.get(i));
-                    break;
-                } else {
-                    tmp /= prime.get(i);
-                    result.add(prime.get(i));
-                    i=0;
-                }
-            } else {
-                i++;
-            }
-
-        }
-
-//        for(int j=0; j<result.size(); j++) {
-//            if(j == result.size()-1) {
-//                sb.append(result.get(j));
-//            } else {
-//                sb.append(result.get(j)).append("\n");
-//            }
-//        }
-//        System.out.println(sb);
-        for (Integer integer : result) {
-            System.out.println(integer);
-        }
-
-
+        System.out.println(sb);
     }
 }
 
