c84d014a63bcaf00579a628bb918e647ee7e4045
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2022-04-20 11:57:16 +0900

    Update 소인수분해.java

--- 백준/Silver/11653. 소인수분해/소인수분해.java
+++ 백준/Silver/11653. 소인수분해/소인수분해.java
@@ -6,6 +6,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        // solution 1
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
@@ -22,6 +23,64 @@ public class Main {
             sb.append(num);
         }
 
+        System.out.println(sb);
+        
+        // solution 2
+        // 에라토스테네스의 채 사용 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+        int tmp = num;
+
+        if(num == 1) {
+            return;
+        }
+
+        boolean[] arr = new boolean[num + 1];
+        arr[0] = true;
+        arr[1] = true;
+        // 에라토스테네스의 채
+        for (int i = 2; i * i < num + 1; i++) {
+            for (int j = i * i; j < num + 1; j += i) {
+                arr[j] = true;
+            }
+        }
+
+        List<Integer> prime = new ArrayList<>();
+        for(int i=2; i<num+1; i++) {
+            if (!arr[i]) {
+                prime.add(i);
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int i = 0;
+        while(true) {
+            if(tmp % prime.get(i) == 0) {
+                //일단 나눠진다는 것
+                if(tmp / prime.get(i) == 1) {
+                    // 소인수 분해 끝난것
+                    result.add(prime.get(i));
+                    break;
+                } else {
+                    tmp /= prime.get(i);
+                    result.add(prime.get(i));
+                    i=0;
+                }
+            } else {
+                i++;
+            }
+
+        }
+
+        for(int j=0; j<result.size(); j++) {
+            if(j == result.size()-1) {
+                sb.append(result.get(j));
+            } else {
+                sb.append(result.get(j)).append("\n");
+            }
+        }
         System.out.println(sb);
     }
 }
