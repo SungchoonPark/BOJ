1f8a06d1aa80cac09cb7fad060179e79b08348d9
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2022-04-27 11:29:49 +0900

    [Silver V] Title: 다리 놓기, Time: 124 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int[][] dp = new int[30][30];
+
+        for(int i=0; i<30; i++) {
+            // 조합의 2번 성질을 이용
+            dp[i][i] = 1;
+            dp[i][0] = 1;
+        }
+
+        for(int i=2; i<30; i++) {
+            for(int j=1; j<30; j++) {
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+            }
+        }
+
+        int num = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<num; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            sb.append(dp[M][N]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+}
+
