5b727b8c6ae5523b6cc1c75a0a175772ed785083
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2022-06-02 11:52:35 +0900

    [Silver III] Title: 문자열 집합, Time: 2628 ms, Memory: 30828 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int result = 0;
+
+        List<String> strings = new ArrayList<>();
+        for(int i=0; i<N; i++) {
+            strings.add(br.readLine());
+        }
+
+        for(int i=0; i<M; i++) {
+            String tmp = br.readLine();
+
+            if(strings.contains(tmp)) {
+                result++;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
+
