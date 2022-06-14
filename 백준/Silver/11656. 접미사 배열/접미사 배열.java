7273305f9978035173b1acbd619d8c117c62a6e4
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2022-06-14 11:37:14 +0900

    [Silver IV] Title: 접미사 배열, Time: 160 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Silver/11656. 접미사 배열/접미사 배열.java
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -12,31 +12,19 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         String word = br.readLine();
-        char[] arr = word.toCharArray();
-        char[] save = arr.clone();
-        char[] tmp = arr.clone();
-        List<String> result = new ArrayList<>();
+
+        String[] arr = new String[word.length()];
+
+        for(int i=0; i<word.length(); i++) {
+            arr[i] = word.substring(i);
+        }
 
         Arrays.sort(arr);
 
-        for (char c : arr) {
-            String s = "";
-            for(int i=0; i<arr.length; i++) {
-                if(c == save[i] && tmp[i] >=97 && tmp[i] <= 122) {
-                    tmp[i] -= 32;
-                    for(int j=i; j<arr.length; j++) {
-                        s += save[j];
-                    }
-                    break;
-                }
-            }
-            result.add(s);
-        }
-
-        Collections.sort(result);
-        for (String s : result) {
+        for (String s : arr) {
             sb.append(s).append("\n");
         }
+
         System.out.println(sb);
     }
 }
