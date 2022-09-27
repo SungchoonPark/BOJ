4fc80cca4724b948a5158d272aaafa693de2f736
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2022-09-27 11:49:07 +0900

    [Silver III] Title: 이친수, Time: 132 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,25 @@
+import javax.management.BadBinaryOpValueExpException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        int n = Integer.parseInt(br.readLine());
+
+        long dp[] = new long[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        System.out.println(dp[n]);
+    }
+}
