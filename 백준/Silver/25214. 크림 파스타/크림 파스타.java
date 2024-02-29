f9b1ec2800fe194955b5854ebefba9e772b0b69c
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-02-29 19:15:55 +0900

    [Silver IV] Title: 크림 파스타, Time: 592 ms, Memory: 47952 KB -BaekjoonHub

--- 백준/Silver/25214. 크림 파스타/크림 파스타.java
+++ 백준/Silver/25214. 크림 파스타/크림 파스타.java
@@ -5,11 +5,13 @@ import java.util.*;
 
 public class Main {
     private static StringTokenizer st;
+    private static StringBuilder sb;
     private static int[] arr;
     private static long[] dp = new long[200001];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         arr = new int[n+1];
 
@@ -20,15 +22,14 @@ public class Main {
         }
 
         dp[1] = 0;
-
+        sb.append(dp[1]).append(" ");
         int min = arr[1];
         for (int i=2; i<=n; i++) {
             if(arr[i] < min) min = arr[i];
             dp[i] = Math.max(dp[i-1], arr[i] - min);
+            sb.append(dp[i]).append(" ");
         }
 
-        for (int i = 1; i <= n; i++) {
-            System.out.print(dp[i] + " ");
-        }
+        System.out.println(sb);
     }
 }
