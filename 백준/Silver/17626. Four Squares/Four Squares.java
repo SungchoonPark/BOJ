d0dde1c83e0f2dc7cd8a5b9044e1da69d984e367
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-03-07 15:59:32 +0900

    [Silver III] Title: Four Squares, Time: 160 ms, Memory: 14880 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private static StringTokenizer st;
+    private static long[] dp = new long[50001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[1] + dp[i-1];
+            for (int j = 2; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
