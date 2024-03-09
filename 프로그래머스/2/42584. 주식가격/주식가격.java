9972381b2d439ffdf877ca8bf137f1891585b9e9
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-03-09 22:06:44 +0900

    [level 2] Title: 주식가격, Time: 23.09 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,22 +1,22 @@
 import java.util.*;
 class Solution {
-    public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        
+    public static int[] solution(int[] prices) {
         Stack<Integer> stack = new Stack<>();
+        int[] result = new int[prices.length];
+
         for(int i=0; i<prices.length; i++) {
-            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                int index = stack.pop();
-                answer[index] = i - index;
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                // 스택의 원소가 짝을 찾은 경우임!!
+                Integer index = stack.pop();
+                result[index] = i - index;
             }
             stack.push(i);
         }
-        
+
         while(!stack.isEmpty()) {
-            int index = stack.pop();
-            answer[index] = prices.length - index - 1;
+            Integer index = stack.pop();
+            result[index] = prices.length - index - 1;
         }
-        
-        return answer;
+        return result;
     }
 }
