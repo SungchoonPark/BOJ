b106c244ab7b4304451485b78379421062e5ce45
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-03-27 10:13:20 +0900

    [Bronze I] Title: ROT13, Time: 128 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.java
+++ 백준/Bronze/11655. ROT13/ROT13.java
@@ -1,36 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        char[] arr = br.readLine().toCharArray();
+        String s= br.readLine();
 
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i] >= 'A' && arr[i] <= 'Z') {
-                if(arr[i] >= 'N') {
-                    arr[i] -= 13;
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c) || c == ' ') {
+                sb.append(c);
+            } else if (Character.isAlphabetic(c)){
+                if (Character.isUpperCase(c)) {
+                    int newC = c + 13;
+                    if (newC > 90) sb.append((char) (newC - 26));
+                    else sb.append((char)newC);
                 } else {
-                    arr[i] += 13;
+                    int newC = c + 13;
+                    if (newC > 122) sb.append((char) (newC - 26));
+                    else sb.append((char)newC);
                 }
             }
-            if(arr[i] >= 'a' && arr[i] <= 'z') {
-                if(arr[i] >= 'n') {
-                    arr[i] -= 13;
-                } else {
-                    arr[i] += 13;
-                }
-            }
-        }
-
-        for(char c : arr) {
-            sb.append(c);
         }
         System.out.println(sb);
+
     }
 }
-
