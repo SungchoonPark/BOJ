4a823a589e0149eee8ea737fcc7400f3698db209
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-04-16 01:14:34 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -2,7 +2,6 @@ import java.util.*;
 class Solution {
     public long solution(long n) {
         long answer = 0;
-        System.out.println("Math.sqrt(n) : " + Math.sqrt(n));
         if(Math.sqrt(n) * 10 % 10 == 0) {
             answer = (int)(Math.sqrt(n)) + 1;
         } else {
