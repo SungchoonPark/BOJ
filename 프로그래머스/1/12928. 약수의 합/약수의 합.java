22b316df78251f3c3faca394c893cedf63231043
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-04-16 00:14:15 +0900

    [level 1] Title: 약수의 합, Time: 0.04 ms, Memory: 65.7 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -2,8 +2,14 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         int sum = 0;
-        for(int i=1; i<=n; i++) {
-            if(n % i == 0) sum += i;
+        for(int i=1; i<=Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                sum += i;
+                int tmp = n / i;
+                if(tmp != i) {
+                    sum += tmp;   
+                }
+            }
         }
         return sum;
     }
