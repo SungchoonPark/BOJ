17b345c203e542fcb2eb88b56e06d5cd2b1f6b44
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-04-25 20:46:32 +0900

    [level 1] Title: 소수 찾기, Time: 17.64 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,20 +1,22 @@
 class Solution {
     boolean[] arr = new boolean[1000001];
     public int solution(int n) {
-        era();
+        era(n);
         int answer = 0;
-        for(int i=1; i<=n; i++) {
+        for(int i=2; i<=n; i++) {
             if(!arr[i]) answer++;
         }
         return answer;
     }
     
-    private void era() {
+    private void era(int n) {
         arr[0] = true;
         arr[1] = true;
-        for(int i=2; i<=arr.length; i++) {
-            for(int j=i*2; j<arr.length; j+=i) {
-                if(!arr[j]) arr[j] = true;
+        for(int i=2; i*i<=n; i++) {
+            if(!arr[i]) {
+                for(int j=i*i; j<=n; j+=i) {
+                    arr[j] = true;
+                }
             }
         }
     }
