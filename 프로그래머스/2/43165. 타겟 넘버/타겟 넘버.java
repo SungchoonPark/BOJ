a0e72da142e1764c3d99c57c0133bb9c659d0816
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-05-28 14:11:22 +0900

    [level 2] Title: 타겟 넘버, Time: 0.36 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,35 +1,19 @@
 import java.util.*;
+
 class Solution {
+    static int cnt = 0;
     public int solution(int[] numbers, int target) {
-        int idx = 0;
-        int cnt = 0;
-        
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(0);
-        
-        while(true) {
-            int size = q.size();
-            
-            if(idx == numbers.length) {
-                for(int i=0; i<size; i++) {
-                    Integer pop = q.poll();
-                    if(pop == target) cnt++;
-                }
-                break;
-            } else {
-                for(int i=0; i<size; i++) {
-                    Integer pop = q.poll();
-                    int a = pop + numbers[idx];
-                    int b = pop - numbers[idx];
-                    // System.out.println("a : " + a + " " + "b : " + b);
-                    q.offer(a);
-                    q.offer(b);
-                }
-                // System.out.println();
-                idx++;
-            }
+        dfs(0, target, numbers, 0);
+        return cnt;
+    }
+    
+    private void dfs(int num, int target, int[] numbers, int idx) {
+        if(idx == numbers.length) {
+            if(num == target) cnt++;
+            return;
         }
         
-        return cnt;
+        dfs(num + numbers[idx], target, numbers, idx+1);
+        dfs(num - numbers[idx], target, numbers, idx+1);
     }
 }
