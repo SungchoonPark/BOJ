fcafa670902a5b4d05662b96e5c5c8ae3d3e636d
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-06-03 15:49:26 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,32 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
-    private static StringTokenizer st;
-    private static int[] arr;
-    private static int n, bag;
+    static StringTokenizer st;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
 
-        while (n >= 3) {
-            if(n % 5 == 0) {
-                bag = bag + (n/5);
-                n = 0;
-                break;
-            } else {
-                n -= 3;
-                bag++;
-            }
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[5001];
+        Arrays.fill(dp, 99999);
+
+        dp[3] = 1;
+        dp[5] = 1;
+
+        for(int i=6; i<=n; i++) {
+            dp[i] = Math.min(dp[i - 3], dp[i - 5]) + 1;
         }
-
-        if (n == 0) System.out.println(bag);
-        else System.out.println(-1);
+        if(dp[n] >= 99999) System.out.println(-1);
+        else System.out.println(dp[n]);
     }
 }
-
-
