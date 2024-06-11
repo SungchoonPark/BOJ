bc9f7c5b340df1ca770c4d764f9da876164c88b3
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-06-11 13:14:54 +0900

    [Bronze V] Title: 직사각형, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        System.out.println(n*m);
+
+    }
+}
