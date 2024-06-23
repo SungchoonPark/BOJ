cd06dacc98d6e8e7a28fe871a9e0d38140e34044
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-06-23 13:17:42 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 1.79 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -1,23 +1,7 @@
 import java.util.*;
 class Solution {
     public int solution(int[] array, int height) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        int result= 0;
-        
-        for(int i : array) {
-            pq.offer(i);
-        }
-        pq.offer(height);
-        int i = 0;
-        while(!pq.isEmpty()) {
-            if(pq.poll() == height) {
-                result = i;
-                break;
-            }
-            
-            i++;
-        }
-        return result;
+        return (int) Arrays.stream(array).filter(value -> value > height).count();
     }
 }
 
