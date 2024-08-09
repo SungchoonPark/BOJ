5a00853f24d56adebeb93d45cf0109e1f21a958d
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-08-09 15:18:43 +0900

    [Gold IV] Title: 부분합, Time: 256 ms, Memory: 24052 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,47 +1,47 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
     static StringTokenizer st;
+    static StringBuilder sb;
+    static int n, s;
+    static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
         st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int s = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
         int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            int tmp = Integer.parseInt(st.nextToken());
+            arr[i] = tmp;
         }
 
         int left = 0;
         int right = 0;
-        int sum = arr[0];
-        int length = 1;
-        int minLength = Integer.MAX_VALUE;
+        int sum = arr[left];
+        while (left <= right) {
+            if (left == n || right == n) break;
+            int tmpLen = right - left + 1;
 
-        while (true) {
-            if (sum < s) {
-                right++;
-                length++;
-                if (right >= n) break;
-                sum += arr[right];
-            }
-            else {
-                length--;
-                minLength = Math.min(minLength, length);
-                sum -= arr[left];
+            if (sum >= s) {
+                if (tmpLen < min) {
+                    min = tmpLen;
+                }
                 left++;
-                if (left >= n || left > right) break;
+                if (left != n) sum -= arr[left - 1];
+            } else {
+                right++;
+                if (right != n) sum += arr[right];
             }
         }
-        if (minLength == Integer.MAX_VALUE) System.out.println(0);
-        else System.out.println(minLength + 1);
+        if (min == Integer.MAX_VALUE) System.out.println(0);
+        else System.out.print(min);
     }
-
 }
