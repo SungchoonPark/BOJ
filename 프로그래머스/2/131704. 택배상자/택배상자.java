706dc8e50f5e788bfa603720639b4120e7716176
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-09-19 16:48:28 +0900

    [level 2] Title: 택배상자, Time: 152.41 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -2,39 +2,23 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] order) {
-        // order -> 택배 기사님이 원하는 상자 순서
-        // return -> 영재가 실을 수 있는 상자 개수
-        int answer = 0;
-        Queue<Integer> q = new LinkedList<>();
-        Stack<Integer> stack = new Stack<>();
-        for(int i = 1; i<= order.length; i++) q.add(i);
+        int idx = 0;
+        
+        Stack<Integer> st = new Stack<>();
         
         for(int i=0; i<order.length; i++) {
-            int wantNum = order[i];
-            boolean flag = false;
+            st.push(i + 1);
             
-            if(!stack.isEmpty()) {
-                if(wantNum == stack.peek()) {
-                    flag = true;
-                    stack.pop();
-                    answer++;
-                    continue;
+            while(!st.isEmpty()) {
+                if(st.peek() == order[idx]) {
+                    st.pop();
+                    idx++;
+                } else {
+                    break;
                 }
             }
-            
-            while(!q.isEmpty()) {
-                Integer curBN = q.poll();
-                if(curBN == wantNum) {
-                    flag = true;
-                    answer++;
-                    break;
-                } 
-                stack.push(curBN);
-            }
-            if(!flag) break;
         }
         
-        
-        return answer;
+        return idx;
     }
 }
