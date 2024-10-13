5dd160605d1a3a994d7a696d96ad8fcfcf893dce
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-10-13 17:04:19 +0900

    [level 2] Title: 점 찍기, Time: 16.14 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -1,11 +1,11 @@
 class Solution {
     public long solution(int k, int d) {
-
-        long count = 0;
-        for(int i = 0 ; i <= d ; i+=k){
-            int Y = (int) Math.sqrt( Math.pow(d,2) - Math.pow(i,2)) ;
-            count += 1 + Y/k;
+        
+        long answer = 0;
+        for(int i=0; i<=d; i+=k) {
+            int y = (int)Math.sqrt(Math.pow(d, 2) - Math.pow(i, 2));
+            answer += 1 + y / k;
         }
-        return count;
+        return answer;
     }
 }
