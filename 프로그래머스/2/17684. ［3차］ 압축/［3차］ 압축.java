4cf1a73a4d51395f265a48137aa8b53841649bb8
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-10-14 22:16:18 +0900

    [level 2] Title: [3차] 압축, Time: 29.81 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -12,28 +12,23 @@ class Solution {
         }
         
         int i = 0;
-        while (i < msg.length()) {
-            // 문자열 탐색 시작
+        while(i < msg.length()) {
             String tmp = String.valueOf(msg.charAt(i));
-            int tmpIdx = dir.get(tmp);
+            Integer tmpIdx = dir.get(tmp);
             int j = i + 1;
             
-            // 더 긴 문자열을 찾을 수 있으면 계속 찾기
-            while (j < msg.length() && dir.containsKey(tmp + msg.charAt(j))) {
+            while(j < msg.length() && dir.containsKey(tmp + msg.charAt(j))) {
                 tmp += msg.charAt(j);
                 tmpIdx = dir.get(tmp);
                 j++;
             }
             
-            // 결과에 현재 찾은 문자열의 인덱스 추가
             result.add(tmpIdx);
             
-            // 새로운 문자열을 사전에 등록 (사전에 없는 경우)
-            if (j < msg.length()) {
+            if(j < msg.length()) {
                 dir.put(tmp + msg.charAt(j), idx++);
             }
             
-            // i를 새롭게 찾은 문자열 길이만큼 증가시켜 다음으로 넘어감
             i = j;
         }
         
