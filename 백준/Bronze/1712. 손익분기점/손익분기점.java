ad29a58f0f72747e1eda577b1c1d4b7ca8ea0ce5
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-10-30 12:05:41 +0900

    [Bronze II] Title: 손익분기점, Time: 104 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if (b >= c) {
+            System.out.println(-1);
+            return;
+        }
+
+        int bep = a / (c - b) + 1;
+
+        System.out.println(bep);
+    }
+}
