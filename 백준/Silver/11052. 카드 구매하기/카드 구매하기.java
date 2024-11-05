b87c9cbe013bd176574740c6e45d94693462f488
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-11-05 18:13:42 +0900

    [Silver I] Title: 카드 구매하기, Time: 128 ms, Memory: 14456 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -2,26 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static StringTokenizer st;
+    static StringBuilder sb;
+    static int result;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int n = Integer.parseInt(br.readLine());
         int[] p = new int[n + 1];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
             p[i] = Integer.parseInt(st.nextToken());
         }
-        
-        int[] dp = new int[n + 1];
-        dp[0] = 0;  // 카드를 구매하지 않을 때의 최대 금액은 0
-        
+
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                dp[i] = Math.max(dp[i], dp[i - j] + p[j]);
+                dp[i] = Math.max(dp[i], dp[i-j] + p[j]);
             }
         }
-        
+
         System.out.println(dp[n]);
     }
 }
