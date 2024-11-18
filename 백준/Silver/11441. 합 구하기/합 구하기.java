4ce2f59bf266bb5e18ce1def7e28c22013468d16
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-11-18 23:38:43 +0900

    [Silver III] Title: 합 구하기, Time: 560 ms, Memory: 56992 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+        int[] prefixSum = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 1; i <= n; i++) {
+            prefixSum[i] = arr[i] + prefixSum[i-1];
+        }
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int result = prefixSum[b] - prefixSum[a-1];
+            sb.append(result).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
