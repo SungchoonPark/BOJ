b70a5485b6cadb9c148d684edb7901e966b4a91a
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-12-12 19:09:07 +0900

    [Silver V] Title: 창문 닫기, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.print((int) Math.sqrt(n));
+    }
+
+}
