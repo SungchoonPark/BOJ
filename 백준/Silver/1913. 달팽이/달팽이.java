8bac15dccffc86e5d32fd69e9fc33d1f1ab70666
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2024-12-27 16:32:14 +0900

    [Silver III] Title: 달팽이, Time: 3988 ms, Memory: 125808 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.java
+++ 백준/Silver/1913. 달팽이/달팽이.java
@@ -2,12 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static StringTokenizer st;
     static int[][] arr;
     static int n, findNum;
-
-    static int resultX;
-    static int resultY;
+    static int resultX, resultY;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,7 +16,7 @@ public class Main {
 
         int startValue = (int) Math.pow(n, 2);
 
-        print(startValue, 0, 0, 0);
+        print(startValue);
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -31,41 +28,50 @@ public class Main {
         System.out.print(resultY + " " + resultX);
     }
 
-    private static void print(int value, int y, int x, int dist) {
-        if (value == 0) return;
-        if (value == findNum) {
-            resultY = y + 1;
-            resultX = x + 1;
-        }
+    private static void print(int value) {
+        int y = 0, x = 0, dist = 0;
 
-        arr[y][x] = value;
-        if (dist == 0) {
-            int ny = y + 1;
-            if (ny < n && arr[ny][x] == 0) {
-                print(value - 1, ny, x, dist);
-            } else {
-                print(value -1, y, x + 1, 1);
+        while (value > 0) {
+            if (value == findNum) {
+                resultY = y + 1;
+                resultX = x + 1;
             }
-        } else if (dist == 1) {
-            int nx = x + 1;
-            if (nx < n && arr[y][nx] == 0) {
-                print(value - 1, y, nx, dist);
-            } else {
-                print(value -1, y - 1, x, 2);
-            }
-        } else if (dist == 2) {
-            int ny = y - 1;
-            if (ny >= 0 && arr[ny][x] == 0) {
-                print(value - 1, ny, x, dist);
-            } else {
-                print(value -1, y, x - 1, 3);
-            }
-        } else if (dist == 3) {
-            int nx = x - 1;
-            if (nx >= 0 && arr[y][nx] == 0) {
-                print(value - 1, y, nx, dist);
-            } else {
-                print(value -1, y + 1, x, 0);
+
+            arr[y][x] = value;
+            value--;
+
+            if (dist == 0) {
+                int ny = y + 1;
+                if (ny < n && arr[ny][x] == 0) {
+                    y = ny;
+                } else {
+                    dist = 1;
+                    x++;
+                }
+            } else if (dist == 1) {
+                int nx = x + 1;
+                if (nx < n && arr[y][nx] == 0) {
+                    x = nx;
+                } else {
+                    dist = 2;
+                    y--;
+                }
+            } else if (dist == 2) {
+                int ny = y - 1;
+                if (ny >= 0 && arr[ny][x] == 0) {
+                    y = ny;
+                } else {
+                    dist = 3;
+                    x--;
+                }
+            } else if (dist == 3) {
+                int nx = x - 1;
+                if (nx >= 0 && arr[y][nx] == 0) {
+                    x = nx;
+                } else {
+                    dist = 0;
+                    y++;
+                }
             }
         }
     }
