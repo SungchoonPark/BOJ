f678c06653bf7ba52737f795ad86340cf86704a5
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2025-07-14 23:49:45 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 100 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -12,24 +12,11 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
 
-        char star = '*';
-        char blank = ' ';
         for (int i = 0; i < n; i++) {
-            int starCount = n - i;
-            int blankCount = i;
-
-            for (int j = 0; j < blankCount; j++) {
-                sb.append(blank);
-            }
-
-            for (int j = 0; j < starCount; j++) {
-                sb.append(star);
-            }
-            if (i != n-1) {
-                sb.append("\n");
-            }
+            sb.append(" ".repeat(i))
+                .append("*".repeat(n - i));
+            if (i != n - 1) sb.append('\n');
         }
-
         System.out.print(sb);
     }
 }
