abb2ae8119035712a0338094282b1593712f96c1
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2025-08-08 11:22:23 +0900

    Time: 22 ms (94.79%), Space: 56.7 MB (91.19%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int[] result = new int[temperatures.length];
-
+        
         Deque<Integer> stack = new ArrayDeque<>();
         for(int i=0; i<temperatures.length; i++) {
             while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
@@ -13,6 +13,7 @@ class Solution {
 
             stack.push(i);
         }
+        
 
         return result;
     }
