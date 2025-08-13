504edeef6f68b0db3040a6ee464ffb3a4a02e8d2
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2025-08-13 15:17:14 +0900

    [Silver III] Title: 병든 나이트, Time: 104 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/1783. 병든 나이트/병든 나이트.java
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.java
@@ -1,13 +1,32 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-<hr><center>nginx/1.22.1</center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static StringTokenizer st;
+    static int n, m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        int result = 0;
+        if (n == 1) {
+            result = 1;
+        } else if (n == 2) {
+            result = Math.min(4, (m + 1) / 2);
+        } else if (n >= 3) {
+            if (m < 7) {
+                result = Math.min(4, m);
+            } else {
+                result = m - 2;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
