c92c3c1a55167bad74243aa6804ce34e47d03626
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2025-08-20 16:18:27 +0900

    [Silver I] Title: 1로 만들기 2, Time: 132 ms, Memory: 22328 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -2,65 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n;
+
+    static StringTokenizer st;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
-        n = Integer.parseInt(st.nextToken());
-        bfs();
-    }
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[1_000_001];
+        int[] path = new int[1_000_001];
 
-    private static void bfs() {
-        Queue<Pos> q = new LinkedList<>();
-        Set<Integer> visited = new HashSet<>();
-        List<Integer> list = new ArrayList<>();
-        list.add(n);
-        q.offer(new Pos(n, list));
-        visited.add(n);
-
-        while(!q.isEmpty()) {
-            Pos cur = q.poll();
-            int curNum = cur.num;
-            List<Integer> curList = cur.list;
-
-            if(curNum == 1) {
-                System.out.println(curList.size() - 1);
-                curList.forEach(num -> System.out.print(num + " "));
-                return;
+        dp[1] = 0;
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = dp[i-1] + 1;
+            path[i] = i-1;
+            if (i % 3 == 0) {
+                int before = dp[i];
+                dp[i] = Math.min(dp[i], dp[i/3] + 1);
+                if(before != dp[i]) {
+                    path[i] = i / 3;
+                }
             }
-
-            if (curNum % 3 == 0 && !visited.contains(curNum / 3)) {
-                List<Integer> newList = new ArrayList<>(curList);
-                newList.add(curNum / 3);
-                q.offer(new Pos(curNum / 3, newList));
-                visited.add(curNum / 3);
-            }
-
-            if (curNum % 2 == 0 && !visited.contains(curNum / 2)) {
-                List<Integer> newList = new ArrayList<>(curList);
-                newList.add(curNum / 2);
-                q.offer(new Pos(curNum / 2, newList));
-                visited.add(curNum / 2);
-            }
-
-            if (!visited.contains(curNum - 1)) {
-                List<Integer> newList = new ArrayList<>(curList);
-                newList.add(curNum - 1);
-                q.offer(new Pos(curNum - 1, newList));
-                visited.add(curNum - 1);
+            if (i % 2 == 0) {
+                int before = dp[i];
+                dp[i] = Math.min(dp[i], dp[i/2] + 1);
+                if(before != dp[i]) {
+                    path[i] = i / 2;
+                }
             }
         }
-    }
 
-    static class Pos {
-        int num;
-        List<Integer> list;
-
-        public Pos(int num, List<Integer> list) {
-            this.num = num;
-            this.list = list;
+        int idx = n;
+        while(dp[idx] != 0) {
+            sb.append(idx).append(" ");
+            idx = path[idx];
         }
+        sb.append(1);
+
+        System.out.println(dp[n]);
+        System.out.print(sb);
+
     }
 }
