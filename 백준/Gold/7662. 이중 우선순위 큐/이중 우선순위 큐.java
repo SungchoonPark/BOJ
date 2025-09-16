2a36b3f4327c5747961357287abebbe31afa12c6
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2025-09-16 16:47:11 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2568 ms, Memory: 361164 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -2,48 +2,62 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    static StringTokenizer st;
+    static StringBuilder sb;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        sb = new StringBuilder();
+        int caseNum = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(br.readLine());
-            TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int i = 0; i < caseNum; i++) {
+            int commandCount = Integer.parseInt(br.readLine());
+            TreeMap<Integer, Integer> tm = new TreeMap<>();
 
-            for (int j = 0; j < n; j++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                String command = st.nextToken();
+            for (int i1 = 0; i1 < commandCount; i1++) {
+                st = new StringTokenizer(br.readLine());
+
+                String com = st.nextToken();
                 int value = Integer.parseInt(st.nextToken());
 
-                if (command.equals("I")) {
-                    // 삽입
-                    map.put(value, map.getOrDefault(value, 0) + 1);
-                } else if (command.equals("D")) {
-                    // 삭제
-                    if (map.isEmpty()) continue;
-
+                if (com.equals("I")) {
+                    if (tm.get(value) != null) {
+                        tm.put(value, tm.get(value) + 1);
+                    } else {
+                        tm.put(value, 1);
+                    }
+                } else if (com.equals("D") && !tm.isEmpty()) {
                     if (value == -1) {
-                        // 최소값 삭제
-                        int minKey = map.firstKey();
-                        if (map.put(minKey, map.get(minKey) - 1) == 1) {
-                            map.remove(minKey);
+                        // 최소값 삭제 (단, 같은 값이 복수개일 경우 하나만 삭제한다)
+                        Integer min = tm.get(tm.firstKey());
+
+                        if (min <= 1) {
+                            tm.remove(tm.firstKey());
+                        } else {
+                            tm.put(tm.firstKey(), tm.get(tm.firstKey()) - 1);
                         }
                     } else {
                         // 최대값 삭제
-                        int maxKey = map.lastKey();
-                        if (map.put(maxKey, map.get(maxKey) - 1) == 1) {
-                            map.remove(maxKey);
+                        Integer max = tm.get(tm.lastKey());
+
+                        if (max <= 1) {
+                            tm.remove(tm.lastKey());
+                        } else {
+                            tm.put(tm.lastKey(), tm.get(tm.lastKey()) - 1);
                         }
                     }
                 }
             }
 
-            // 결과 출력
-            if (map.isEmpty()) {
-                System.out.println("EMPTY");
+            if (tm.isEmpty()) {
+                sb.append("EMPTY").append("\n");
             } else {
-                System.out.println(map.lastKey() + " " + map.firstKey());
+                sb.append(tm.lastKey()).append(" ").append(tm.firstKey()).append("\n");
             }
         }
+        System.out.print(sb);
+
     }
+
 }
