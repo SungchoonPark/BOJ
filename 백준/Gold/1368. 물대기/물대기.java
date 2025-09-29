848d326dd93c52f968072dd03c30d4cb589c210a
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2025-09-29 12:11:07 +0900

    [Gold II] Title: 물대기, Time: 292 ms, Memory: 26412 KB -BaekjoonHub

---
+++ 백준/Gold/1368. 물대기/물대기.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static StringTokenizer st;
+    static StringBuilder sb;
+
+    static int n;
+    static int[] parent;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Link> pq = new PriorityQueue<>(Comparator.comparingInt(l -> l.cost));
+        parent = new int[n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < n; i++) {
+            int w = Integer.parseInt(br.readLine());
+            pq.add(new Link(i, n, w));
+        }
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int i1 = 0; i1 < n; i1++) {
+                int cost = Integer.parseInt(st.nextToken());
+                if (i == i1) continue;
+
+                pq.add(new Link(i, i1, cost));
+            }
+        }
+
+        int count = 0;
+        int result = 0;
+        while(!pq.isEmpty()) {
+            Link minLink = pq.poll();
+
+            int p1 = find(minLink.to);
+            int p2 = find(minLink.from);
+
+            if (p1 == p2) continue;
+
+            union(p1, p2);
+            count++;
+            result += minLink.cost;
+            if (count == n) break;
+        }
+
+        System.out.println(result);
+
+    }
+
+    private static int find(int node) {
+        if (parent[node] == node) return node;
+        return parent[node] = find(parent[node]);
+    }
+
+    private static void union(int a, int b) {
+        int max = Math.max(a, b);
+        int min = Math.min(a, b);
+
+        parent[max] = min;
+    }
+
+    static class Link {
+        int to;
+        int from;
+        int cost;
+
+        public Link(int to, int from, int cost) {
+            this.to = to;
+            this.from = from;
+            this.cost = cost;
+        }
+    }
+}
