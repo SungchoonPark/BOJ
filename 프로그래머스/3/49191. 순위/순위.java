9a9253e6b3737ba863bce4cf2c57c71d0f46fa50
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2025-10-29 17:14:44 +0900

    [level 3] Title: 순위, Time: 12.11 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,47 +1,32 @@
 class Solution {
     public int solution(int n, int[][] results) {
-        boolean[][] graph = new boolean[n][n];
-        for (int[] edge : results) {
-            // 인덱스는 0부터 시작이니까 1을 빼줌
-            int u = edge[0] - 1;
-            int v = edge[1] - 1;
-            // 이는 u는 v에게 항상 이긴다는 것
-            graph[u][v] = true;
+        
+        boolean[][] win = new boolean[n][n];
+        for (int[] r : results) {
+            win[r[0]-1][r[1]-1] = true;
         }
-
-        int count = 0;
-        for(int u = 0; u<n; u++) {
-            int wins = countForward(u, graph, new boolean[n]) - 1;
-            int loses = countBackward(u, graph, new boolean[n]) - 1;
-            if (wins + loses + 1 == n) {
-                count++;
+        
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (win[i][k] && win[k][j]) {
+                        win[i][j] = true;
+                    }
+                }
             }
         }
-
-        return count;
-    }
-
-    private int countBackward(int u, boolean[][] graph, boolean[] isVisited) {
-        int count = 1;
-
-        for(int v = 0; v < graph.length; v++) {
-            if (!graph[v][u] || isVisited[v]) continue;
-            isVisited[v] = true;
-            count += countBackward(v, graph, isVisited);
+        
+        int answer = 0;
+        for(int i = 0; i < n; i++) {
+            int know = 0;
+            for(int j=0; j<n; j++) {
+                if(i == j) continue;
+                if(win[i][j] || win[j][i]) know++;
+            }
+            
+            if(know == n - 1) answer++;
         }
-
-        return count;
-    }
-
-    private int countForward(int u, boolean[][] graph, boolean[] isVisited) {
-        int count = 1;
-
-        for(int v = 0; v < graph[u].length; v++) {
-            if (!graph[u][v] || isVisited[v]) continue;
-            isVisited[v] = true;
-            count += countForward(v, graph, isVisited);
-        }
-
-        return count;
+        
+        return answer;
     }
 }
