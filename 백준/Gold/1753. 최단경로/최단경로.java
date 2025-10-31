2206f5360c6e66a766a13d6e50540e2ed240b349
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2025-10-31 16:48:09 +0900

    [Gold IV] Title: 최단경로, Time: 648 ms, Memory: 108472 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,74 +1,85 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
+
     static StringTokenizer st;
     static List<Node>[] graph;
-    static StringBuilder sb;
     static int[] dist;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         st = new StringTokenizer(br.readLine());
-        sb = new StringBuilder();
-
-        int V = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
         int e = Integer.parseInt(st.nextToken());
-        dist = new int[V + 1];
-        graph = new List[V + 1];
+        graph = new List[v + 1];
+        dist = new int[v + 1];
+        
+        Arrays.fill(dist, Integer.MAX_VALUE);
 
-        for(int i=0; i<V+1; i++) graph[i] = new ArrayList<>();
-        for(int i=0; i<V+1; i++) dist[i] = Integer.MAX_VALUE;
+        for (int i = 0; i < v + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
-        int k = Integer.parseInt(br.readLine());
+        int start = Integer.parseInt(br.readLine());
+        dist[0] = 0;
+        dist[start] = 0;
 
-        for(int i=0; i<e; i++) {
+        for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
 
             int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
 
-            graph[u].add(new Node(v, w));
+            graph[u].add(new Node(n, w));
         }
-
-        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.cost));
-
-        pq.offer(new Node(k, 0));
-        dist[k] = 0;
-
-        while(!pq.isEmpty()) {
-            Node curNode = pq.poll();
-
-            if (dist[curNode.idx] < curNode.cost) {
-                continue;
+        
+        dijkstra(start);
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < v + 1; i++) {
+            int d = dist[i];
+            
+            if (d == Integer.MAX_VALUE) {
+                sb.append("INF").append("\n");
+            } else {
+                sb.append(d).append("\n");
             }
-
-            for (Node nextNode : graph[curNode.idx]) {
-                if (dist[nextNode.idx] > curNode.cost + nextNode.cost) {
-                    dist[nextNode.idx] = curNode.cost + nextNode.cost;
-                    pq.offer(new Node(nextNode.idx, dist[nextNode.idx]));
-                }
-            }
-        }
-
-        for (int i=1; i<dist.length; i++) {
-            if (dist[i] == Integer.MAX_VALUE) sb.append("INF").append("\n");
-            else sb.append(dist[i]).append("\n");
         }
 
         System.out.print(sb);
     }
-}
+    
+    private static void dijkstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> n1.cost - n2.cost);
+        pq.add(new Node(start, 0));
+        
+        while(!pq.isEmpty()) {
+            Node curNode = pq.poll();
 
-class Node {
-    int idx;
-    int cost;
+            if (dist[curNode.node] < curNode.cost) continue;
 
-    public Node(int idx, int cost) {
-        this.idx = idx;
-        this.cost = cost;
+            for (Node next : graph[curNode.node]) {
+                int newDist = dist[curNode.node] + next.cost;
+
+                if (dist[next.node] > newDist) {
+                    dist[next.node] = newDist;
+                    pq.offer(new Node(next.node, newDist));
+                }
+            }
+        }
+
+    }
+
+    static class Node {
+        int node;
+        int cost;
+
+        public Node(int node, int cost) {
+            this.node = node;
+            this.cost = cost;
+        }
     }
 }
