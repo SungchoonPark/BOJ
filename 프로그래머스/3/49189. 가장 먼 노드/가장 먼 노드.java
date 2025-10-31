7659444fdbd683143edf0ead4f60859e370415d8
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2025-10-31 17:53:02 +0900

    [level 3] Title: 가장 먼 노드, Time: 56.81 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,61 +2,61 @@ import java.util.*;
 
 class Solution {
     static List<Integer>[] graph;
+    static int result;
     static boolean[] visited;
     
     public int solution(int n, int[][] edge) {
-        int answer = 0;
         graph = new List[n + 1];
-        visited = new boolean[n+1];
-        
-        for(int i=0; i<n + 1; i++) {
+        visited = new boolean[n + 1];
+        for(int i=0; i<n+1; i++) {
             graph[i] = new ArrayList<>();
         }
         
         for(int i=0; i<edge.length; i++) {
-            int a1 = edge[i][0];
-            int a2 = edge[i][1];
+            int a = edge[i][0];
+            int b = edge[i][1];
             
-            graph[a1].add(a2);
-            graph[a2].add(a1);
+            graph[a].add(b);
+            graph[b].add(a);
         }
         
-        return bfs();
+        visited[1] = true;
+        bfs(1);
+        
+        return result;
     }
     
-    public int bfs() {
-        Queue<Info> q = new LinkedList<>();
-        q.offer(new Info(1, 1));
-        int maxDepth = 1;
-        int count = 0;
-        visited[1] = true;
+    private void bfs(int start) {
+        Queue<NodeInfo> q = new LinkedList<>();
+        q.add(new NodeInfo(start, 0));
+        
+        int maxDepth = -1;
         
         while(!q.isEmpty()) {
-            Info curInfo = q.poll();
+            NodeInfo curNode = q.poll();
             
-            if(curInfo.depth == maxDepth) {
-                count++;
-            } else if(curInfo.depth > maxDepth) {
-                maxDepth = curInfo.depth;
-                count = 1;
+            if(curNode.depth > maxDepth) {
+                maxDepth = curNode.depth;
+                result = 1;
+            } else if(curNode.depth == maxDepth) {
+                result++;
             }
             
-            for(int n : graph[curInfo.node]) {
-                if(!visited[n]) {
-                    visited[n] = true;
-                    q.offer(new Info(n, curInfo.depth + 1));
+            for(Integer a : graph[curNode.node]) {
+                if(!visited[a]) {
+                    visited[a] = true;
+                    q.offer(new NodeInfo(a, curNode.depth + 1));
                 }
             }
         }
-        return count;
     }
 }
 
-class Info {
+class NodeInfo {
     int node;
     int depth;
     
-    public Info(int node, int depth) {
+    public NodeInfo(int node, int depth) {
         this.node = node;
         this.depth = depth;
     }
